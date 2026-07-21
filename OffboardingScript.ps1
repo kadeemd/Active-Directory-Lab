25782ea5fdd448c8bc99ff9d37103d05af5276cd
kadeemd <kadeemdonovan@gmail.com>
2026-07-21 12:50:56 +0100

    Update OffboardingScript.ps1
    This script disables user accounts and moves them into a "Disabled Users" Organizational Unit.

--- OffboardingScript.ps1
+++ OffboardingScript.ps1
@@ -4,6 +4,9 @@ Import-Module ActiveDirectory
 $CSVPath = "C:\OffboardEmployees.csv"
 $Users = Import-Csv -Path $CSVPath
 
+#Define target OU.
+$TargetOU = "OU=Disabled Users,DC=kadtech,DC=local"
+
 # Display users that are within the CSV file.
 Write-Host "[*] Users detected in CSV file: $($Users.Username -join ', ')" -ForegroundColor Cyan
 
@@ -13,7 +16,8 @@ foreach ($Row in $Users) {
 
 	#Get user identity from active directory and produce error if user not found.
 	$ADUser = Get-ADUser -Identity $Username -ErrorAction SilentlyContinue
-
+        
+        #Error check if user does not exist within Active Directory.
 	if (-not $ADUser) {
         Write-Host "[-] ERROR: User $Username not found in Active Directory." -ForegroundColor Red
 		continue 
@@ -22,4 +26,15 @@ foreach ($Row in $Users) {
 	#Disable the users account
 	Disable-ADAccount -Identity $ADUser
 	Write-Host "[+] SUCCESS: Disabled user account for $Username" -ForegroundColor Green
+
+	#Move disabled accounts into Disabled Users OU.
+        try{
+	  Move-ADObject -Identity $ADUser.DistinguishedName -TargetPath $TargetOU
+          Write-Host "Successfully moved: $($Username)" -ForegroundColor Green
+	 }
+        
+        #Error check if users failed to be moved into new OU.
+        catch {
+        Write-Host "[-] ERROR: Failed to move $Username. Error: $_" -ForegroundColor Red
+        }
 }
